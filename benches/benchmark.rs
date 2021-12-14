8788f034aac889899d05e4cedb056264cb3b5b20
Nicholas L <nicholas-l@users.noreply.github.com>
2021-12-14 21:08:49 +0000

    FIx benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,16 +1,20 @@
-use advent_of_code_2021::{get_day, get_days, get_data};
+use advent_of_code_2021::{get_data, get_day, get_days};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     for i in get_days() {
         let (day1, day2, path) = get_day(i);
-        c.bench_function(&format!("day {} A", i), |b| b.iter(|| {
-            day1(get_data(&path));
-        }));
+        c.bench_function(&format!("day {} A", i), |b| {
+            b.iter(|| {
+                day1(get_data(&path));
+            })
+        });
 
-        c.bench_function(&format!("day {} B", i), |b| b.iter(|| {
-            day2(get_data(&path));
-        }));
+        c.bench_function(&format!("day {} B", i), |b| {
+            b.iter(|| {
+                day2(get_data(&path));
+            })
+        });
     }
 }
 
